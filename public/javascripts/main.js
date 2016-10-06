a1cb08310f340cb174d46390eb00e4350f3570cf
Rajesh Veluchamy <Rajesh@Rajeshs-MacBook-Pro.local>
2016-10-06 11:55:34 +0100

    scroll to top page on guidelines page added with custom scripts

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,31 @@
 $( document ).ready(function() {
 
-
-
-
+// ===== Scroll to Top Script ==== 
+$('#return-to-top').addClass('hideScroller'); // hide on load
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').addClass('showScroller');
+         $('#return-to-top').removeClass('hideScroller');
+    } 
 });
+
+$('#return-to-top').click(function() {      // When return to top arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0    // Scroll to top of body
+    }, 500);
+     $('#return-to-top').removeClass('showScroller'); //remove show
+   	 $('#return-to-top').addClass('hideScroller'); // add hide
+       
+   
+});
+
+$(window).scroll(function() { // if page is scrolled to the top
+    if ($(this).scrollTop() === 0 ) {    
+        $('#return-to-top').removeClass('showScroller');
+         $('#return-to-top').addClass('hideScroller');
+    } 
+});
+
+
+
+}); //document.ready
